6cc6123ac4c038a8bc1e83b4173eecc16f07c5c2
SALIM FAID <salimfaid70@gmail.com>
2024-12-19 20:14:54 -0500

    completed webdev-js-intro-02

--- js/assignment.js
+++ js/assignment.js
@@ -1,30 +1,43 @@
 "use strict";
 
-// Declaring constant variables to store references to the elements
-// that will be updated by your code.
-const yearElement = document.getElementById("year");
-const resolutionElement = document.getElementById("resolution")
-const willMeetResolutionElement = document.getElementById("will-meet-resolution")
-const submissionBtn = document.getElementById("submission-btn")
+// I Create an immutable variable named resolution and assign it a value
+const resolution = "Exercise daily";
 
-// Declare your variables here.
+// I Create an immutable variable named currentYear and assign it a value
+const currentYear = 2025;
 
+// I Create a mutable variable named willMeetResolution and assign it a value
+let willMeetResolution = true;
+
+// Function to update the year element
 function updateYear() {
-    // write the logic
-
+    const yearElement = document.getElementById("year");
+    yearElement.innerText = `The year is ${currentYear}`;
 }
 
+// Function to update the resolution element
 function updateResolution() {
-    // Update this function 
-
+    const resolutionElement = document.getElementById("resolution");
+    resolutionElement.innerText = `My resolution: ${resolution}`;
 }
 
+// Function to update the willMeetResolution element
+function updateWillMeetResolution() {
+    const willMeetResolutionElement = document.getElementById("will-meet-resolution");
+    willMeetResolutionElement.innerText = `Will I meet my resolution? ${willMeetResolution}`;
+}
+
+// Function to render all the updates
 function render() {
-    // Finish writing this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 }
 
+// Event listener for the submission button
+const submissionBtn = document.getElementById("submission-btn");
 submissionBtn.addEventListener("click", function () {
-    // Finish writing this function
+    render();
+});
 
-})
+
--- js/index.js
+++ js/index.js
@@ -1,4 +1,47 @@
-// Prevent us from attempting to use variables
-// that are not declared
+
 "use strict"
 
+const myFavoriteColor= "yellow";
+console.log(myFavoriteColor);          
+console.log(typeof myFavoriteColor);   
+
+const myAge = 30;
+console.log(myAge);                    
+console.log(typeof myAge); 
+
+let isLearningJavaScript = true;
+console.log(isLearningJavaScript);     
+console.log(typeof isLearningJavaScript); 
+
+isLearningJavaScript = false;
+console.log(isLearningJavaScript);     
+console.log(typeof isLearningJavaScript); 
+
+
+let myHobby;
+console.log(myHobby);                  
+console.log(typeof myHobby);           
+
+
+const noValue = null;
+console.log(noValue);                 
+console.log(typeof noValue);           
+
+
+const uniqueId = Symbol("id");
+console.log(uniqueId);                 
+console.log(typeof uniqueId);          
+
+const bigNumber = BigInt(900719925);
+console.log(bigNumber);                
+console.log(typeof bigNumber);         
+
+
+// in this experiment code i used:
+// string data type(favorite color,yellow)
+// used number data type (myage,30)
+// used boolean data type (logical value,true/flase)
+// used undefined data type(myhobby)
+// used null data type(no value,null)
+// used symbol data type(unique id, id)
+// bigint data type(precision,900719925)
